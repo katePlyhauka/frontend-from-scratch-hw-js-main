9b2fe7c811dc25de27ca00b8458cb040f06e5065
katyPlyhauka <pogudinak.e@gmail.com>
2024-12-30 19:37:18 +0300

    homework4-4

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -1,5 +1,6 @@
 /*
-Напишите функцию `doubleEachCharacter`, которая принимает строку и возвращает новую строку, где каждый символ из исходной строки повторяется дважды.
+Напишите функцию `doubleEachCharacter`, которая принимает строку и возвращает новую строку, 
+где каждый символ из исходной строки повторяется дважды.
 
 Входные данные:
 
@@ -12,4 +13,12 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {
+    let newString=''
+  for (let i = 0; i < str.length; i++) {
+      newString=newString+str[i]+str[i]
+  }
+  return newString
+}
+
+
